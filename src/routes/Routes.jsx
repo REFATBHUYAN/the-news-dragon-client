0a7d4e65ac00c2d354fa6c40f51151d67b4c736c
REFATBHUYAN <refatbubt@gmail.com>
2023-04-23 11:20:53 +0600

    news daynamic route complete

--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -1,17 +1,36 @@
 import { createBrowserRouter } from "react-router-dom";
 import Main from "../layout/Main";
 import Home from "../pages/home/Home";
+import Catagory from "../pages/home/Catagory";
+import NewsLayout from "../layout/NewsLayout";
+import News from "../pages/News/News";
 
 const router = createBrowserRouter([
-    {
-        path: '/',
-        element: <Main></Main>,
-        children: [
-            {
-                path: '/',
-                element: <Home></Home>
-            }
-        ]
-    }
-])
+  {
+    path: "/",
+    element: <Main></Main>,
+    children: [
+      {
+        path: "/",
+        element: <Home></Home>,
+      },
+      {
+        path: "/catagory/:id",
+        element: <Catagory></Catagory>,
+        loader: ({ params }) =>
+          fetch(`http://localhost:5000/categories/${params.id}`),
+      },
+    ],
+  },
+  {
+    path: "/news",
+    element: <NewsLayout></NewsLayout>,
+    children: [
+      {
+        path: ":id",
+        element: <News></News>,
+      },
+    ],
+  },
+]);
 export default router;
